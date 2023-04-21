7113853e9027d144ab3ea059082dfb46f2d3e00e
catherinekimani <catherinekimani882@gmail.com>
2023-04-21 22:27:09 +0300

    Refactor function to use tokenized arguements and handle errors

--- execute_command.c
+++ execute_command.c
@@ -13,23 +13,29 @@ void execute_command(char *command_with_args)
 	int num_args = 0;
 	pid_t pid;
 
-	args = tokenize_command(command_with_args, &num_args);
+	args = tokenize_command(command_with_args," ",&num_args);
 	if (args == NULL)
 		return;
 
 	pid = fork();
 	if (pid == -1)
 	{
-		handle_error();
-		free(args);
-		return;
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
 	}
 
 	if (pid == 0)
 	{
-		execute(args);
+		if (execvp(args[0], args) == -1)
+		{
+			perror("Exec failed");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
+	{
 		wait(NULL);
+	}
+
 	free(args);
 }
